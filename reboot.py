f899cefd6af121c7810a94e2ba6b2b81904c675d
Albin Eriksson <40474222+kezoponk@users.noreply.github.com>
2020-07-31 12:40:42 +0200

    Update reboot.py

--- reboot.py
+++ reboot.py
@@ -12,6 +12,7 @@ if_contains_then_invalid = []
 def rebootzyxel():
     # Create a session
     s = requests.Session()
+    
     # Post login form
     response = s.post(target_url+'/login.cgi', allow_redirects=False, data={
     	'UserName': target_username,
@@ -23,13 +24,16 @@ def rebootzyxel():
     print('Trying password '+target_password+' on '+target_username)
     contains_valid = any(if_contain_then_valid in str(response.content) for if_contain_then_valid in if_contains_then_valid)
     contains_invalid = any(if_contain_then_invalid in str(response.content) for if_contain_then_invalid in if_contains_then_invalid)
+    
     # If user & password correct then login
     if contains_valid and not contains_invalid:
         # Get reboot page
         r = s.get(target_url+'/rpSysReboot.cgi')
+        
         # Turn page into a dictionary
         soup = BeautifulSoup(r.text, features="html.parser")
         sessionKey = soup.find('input',attrs = {'name':'sessionKey'})['value']
+        
         # Posting with the right data
         output = s.post(target_url+'/rpSysReboot.cgi', data={
             'sessionKey': sessionKey,
