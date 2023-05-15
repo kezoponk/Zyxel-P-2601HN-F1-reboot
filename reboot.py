49d61a234463aaa2a677cf50072375b063dd253d
Albin Eriksson <albineriksson100@gmail.com>
2023-05-15 14:17:27 +0200

    refactor: use args in reboot_zyxel

--- reboot.py
+++ reboot.py
@@ -6,15 +6,15 @@ target_username = 'user'
 target_password = '9A4H49YTFHRK9'
 target_url = 'http://192.168.1.254'
 
-def reboot_zyxel():
+def reboot_zyxel(username, password, url):
     # Start session
     session = requests.Session()
     
     # Post login form
-    print('Trying password '+target_password+' on '+target_username)
-    response = session.post(target_url+'/login.cgi', allow_redirects=False, data={
-    	'UserName': target_username,
-    	'hiddenPassword': target_password,
+    print('Trying password {} on {}'.format(password, username))
+    response = session.post('{}/login.cgi'.format(url), allow_redirects=False, data={
+    	'UserName': username,
+    	'hiddenPassword': password,
         'loginPassword': "ZyXEL ZyWALL Series",
         'submitValue': "1",
         'Submit': "Login"
@@ -23,18 +23,18 @@ def reboot_zyxel():
     # If user & password correct then login
     if '<script>window.location="indexMain.cgi"</script>' in str(response.content):
         # Get reboot page
-        reboot_page = session.get(target_url+'/rpSysReboot.cgi')
+        reboot_page = session.get('{}/rpSysReboot.cgi'.format(url))
         
         # Parse html to be accessed as dictionary
         soup = BeautifulSoup(reboot_page.text, features="html.parser")
-        session_key = soup.find('input',attrs = {'name':'sessionKey'})['value']
+        session_key = soup.find('input', attrs = {'name':'sessionKey'})['value']
         
         # Post with required values
-        output = session.post(target_url+'/rpSysReboot.cgi', data={
+        output = session.post('{}/rpSysReboot.cgi'.format(url), data={
             'sessionKey': session_key,
             'isReset': 1
         })
         print('Router rebooting')
 
 if __name__ == "__main__":
-    reboot_zyxel()
+    reboot_zyxel(target_username, target_password, target_url)
